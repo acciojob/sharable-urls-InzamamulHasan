65e14b9dbfffdc2cd3f4ecb9df428121669fa21e
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-03-31 16:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+const fors=document.getElementById("ford");
+fors.addEventListener("submit",function(e){
+	e.preventDefault();
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+	let h3=document.querySelector("h3").innerText;
+	let name2="name";
+	let ans="";
+	if(name=="" && year!=""){
+		ans+=h3+"?"+"year="+year;
+	}else if(name!="" && year==""){
+		ans+=h3+"?"+"name="+name;
+	}else if(name!="" && year!=""){
+		ans+=h3+"?"+"name="+name +"&year="+year;
+	}
+	if(ans){
+		document.getElementById("data").textContent=ans;
+	}
+});
+
